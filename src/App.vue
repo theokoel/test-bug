0a5521d4897741e6fc96093bd372ae090929f666
Theo Koel <koeltheo@gmail.com>
2022-08-10 16:57:11 +0200

    Bug : default config

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <span class="material-icons-outlined md-24">power_settings_new</span>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+
 </script>
 
 <style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import '@/css/material-icons.css'
 
 createApp(App).mount('#app')
